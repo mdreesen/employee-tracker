44bd896fedc493422302fe8c2876ba4f6c1fb964
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-10 23:11:48 -0600

    changed file name server to index

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-// TODO
-// need to create inquirer here
+const inquirer = require('inquirer');
+const connection = require('./db/database');
+const employee = require('./lib/employee');
+const department = require('./lib/department');
+const role = require('./lib/role');
+/*
+const startQuestion = () => {
+    return inquirer.prompt({
+            type: 'list',
+            name: 'start',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Role', 'Add An Employee', 'Update Employee Role']
+        })
+        .then(function(answer) {
+            if (answer === 'View All Departments') {
+                getDepartment();
+            }
+        })
+};
+*/
+
+
+//startQuestion()
+// getEmployee();
+// updateEmployee();
+// deleteEmployee();
