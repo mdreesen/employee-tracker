20b6e3074ba9c582a7cd3c0e737bafb425f5a105
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-11 00:20:03 -0600

    added more questions and funtions to index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connection = require('./db/database');
 const employee = require('./lib/employee');
 const department = require('./lib/department');
 const role = require('./lib/role');
-/*
+
 const startQuestion = () => {
     return inquirer.prompt({
             type: 'list',
@@ -13,14 +13,48 @@ const startQuestion = () => {
         })
         .then(function(answer) {
             if (answer === 'View All Departments') {
-                getDepartment();
+                viewDepartments();
+            } else if (answer === 'View All Roles') {
+                return viewRoles();
+            } else if (answer === 'View All Employees') {
+                return viewEmployee();
+            } else if (answer === 'Add A Role') {
+                return addARole();
+            } else if (answer === 'Add An Employee') {
+                return addAnEmployee();
+            } else if (answer === 'Update Employee Role') {
+                return updateAnEmployee();
             }
         })
 };
-*/
+
+const viewDepartments = () => {
+    return getDepartment();
+}
+
+const viewRoles = () => {
+    return getRole();
+}
+
+const viewEmployee = () => {
+    return getEmployee();
+}
+
+const addARole = () => {
+    return addRole();
+}
+
+const addAnEmployee = () => {
+    return AddEmployee();
+}
+
+const updateAnEmployee = () => {
+    return updateEmployee();
+}
 
 
-//startQuestion()
+
+startQuestion();
 // getEmployee();
 // updateEmployee();
 // deleteEmployee();
