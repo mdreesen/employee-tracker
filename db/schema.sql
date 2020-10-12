37618961252cb4a2bc6138d50f440d6e8f377040
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-11 22:19:20 -0600

    updated the schema file to have proper names

--- db/schema.sql
+++ db/schema.sql
@@ -6,20 +6,17 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
-
 -- Employee Table
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     job_title VARCHAR(30) NOT NULL,
-    role_id VARCHAR(30),
-    manager_id VARCHAR(30)
+    role_id INTEGER UNSIGNED,
+    manager_name VARCHAR(30)
 );
 
-
 -- Role Table
-
 CREATE TABLE role (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
